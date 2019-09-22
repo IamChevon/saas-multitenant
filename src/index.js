c2c26b3a4dae448a7526e7a71252dcd040723046
IamChevon <w2chevon@gmail.com>
2019-09-22 00:49:15 -0500

    import routecontroller to access to middle ware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routeController from './routes/route';
 
 const port = 8000;
 
@@ -8,6 +9,9 @@ const app = express();
 app.set('port', port);
 
 app.use(bodyParser.json());
+
+app.use('/', routeController);
+
 
 app.listen(port, () => {
     console.log(`Appliation started on port: ${port}`);
