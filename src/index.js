5544fc84431b1975081a1476c014a6870177825b
IamChevon <w2chevon@gmail.com>
2019-09-28 12:11:55 -0500

    imported required resources. setup path to view tenant info

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routeController from './routes/route';
 
+import './env';
+import * as userService from './services/users';
+import { connectAllDb } from './connectionManager';
+import * as connectionResolver from './middlewares/connectionResolver';
+
+
 const port = 8000;
 
 const app = express();
@@ -10,7 +16,14 @@ app.set('port', port);
 
 app.use(bodyParser.json());
 
+connectAllDb();
+
+app.use(connectionResolver.resolve);
+
 app.use('/', routeController);
+
+// API Route for getting users
+app.get('/users', userService.getAll);
 
 
 app.listen(port, () => {
